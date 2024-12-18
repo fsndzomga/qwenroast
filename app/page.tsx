c63fc47f7bf48f3fb9b9f038883797aec801c3b6
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2024-12-18 17:47:44 +0100

    adding analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import RoastForm from '@/components/RoastForm'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <RoastForm />
       </main>
       <Footer />
+      <Analytics />
     </div>
   )
 }
